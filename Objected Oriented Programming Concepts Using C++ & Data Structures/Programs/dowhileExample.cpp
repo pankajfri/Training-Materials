230b3c539284c4eece2a521731ae90c3c664607c
JishnuTU <jishnutu1995@gmail.com>
2019-06-21 07:17:46 +0530

    oops session 1

---
+++ Objected Oriented Programming Concepts Using C++ & Data Structures/Programs/dowhileExample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // An example of a standard do-while loop
+    int i = 1;
+    do
+    { 
+        cout << i << '\n';
+         i++;
+    }while (i < 10);
+return 0;
+}
