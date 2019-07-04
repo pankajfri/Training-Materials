3f1abdc1da87f06305d598fc10702deacf5b5370
JishnuTU <jishnutu1995@gmail.com>
2019-07-04 18:35:25 +0530

    oops pgms session 3

---
+++ Objected Oriented Programming Concepts Using C++ & Data Structures/Programs/Session1/whileExample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // An example of a standard while loop
+    int i = 1;
+    while (i < 10)
+    { 
+        cout << i << '\n';
+         i++;
+    }
+return 0;
+}
